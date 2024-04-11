5eb0f14b84f914ebded4d71e78c07278dfa44eb2
mihlali1922 <128313700+mihlali1922@users.noreply.github.com>
2024-04-11 14:20:36 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//import all the other classes 
+namespace POE1ST10384498
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {//make recpipie obj 
+            //recipeInfo r= new recipeInfo();
+
+           //if 
+            //have a menue using switch case 
+            //Console writeline("\n1 Create recipe, display full recipie , scale recipe ,alter recipe ,clear data end program)
+            //int userOption =ConertTo
+            //int enters =
+
+        }
+    }
+}
---
+++ recipies.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POE1ST10384498
+{
+    internal class recipies
+    {
+        //methos for creating recipies viewing the recipe and scaling and clearing 
+        //calling /invoking these 
+        //this method must ask 
+        //public string NameIngredients{get};
+        //Array[] name = new Array[] {}
+
+        // for(i)
+    }
+}
